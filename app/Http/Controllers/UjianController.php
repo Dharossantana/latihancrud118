98ee0b8a1777ed3c6ca5c5c987fcff945294546e
wiraaditya20 <wiraaditya555@gmail.com>
2021-04-26 16:44:59 +0800

    upload awal

---
+++ app/Http/Controllers/UjianController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\ujian;
+class UjianController extends Controller
+{
+    public function index()
+    {
+    	$ujian = ujian::all();
+    	return view('ujian', ['ujian' => $ujian]);
+    }
+    public function tambah()
+    {
+    	return view('ujian_tambah');
+    }
+    public function store(Request $request)
+    {
+    	$this->validate($request,[
+    		'nama_mk' => 'required',
+            'dosen' => 'required',
+            'jumlah_soal' => 'required',
+    		'keterangan' => 'required'
+    	]);
+ 
+        ujian::create([
+    		'nama_mk' => $request->nama_mk,
+            'dosen' => $request->dosen,
+            'jumlah_soal' => $request->jumlah_soal,
+    		'keterangan' => $request->keterangan
+    	]);
+ 
+    	return redirect('/ujian');
+    }
+    public function edit($id)
+{
+   $ujian = Ujian::find($id);
+   return view('ujian_edit', ['ujian' => $ujian]);
+}
+public function update($id, Request $request)
+{
+    $this->validate($request,[
+        'nama_mk' => 'required',
+        'dosen' => 'required',
+        'jumlah_soal' => 'required',
+        'keterangan' => 'required'
+    ]);
+
+    $ujian = Ujian::find($id);
+    $ujian->nama_mk = $request->nama_mk;
+    $ujian->dosen = $request->dosen;
+    $ujian->jumlah_soal = $request->jumlah_soal;
+    $ujian->keterangan = $request->keterangan;
+    $ujian->save();
+    return redirect('/ujian');
+}
+public function delete($id)
+{
+    $ujian = ujian::find($id);
+    $ujian->delete();
+    return redirect('/ujian');
+}
+}
